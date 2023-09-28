91c5e9519ad32664df2df3403abdc8bd78de4936
Dmitry Soroko <D.Saroka@sam-solutions.com>
2023-09-28 20:05:43 +0300

    init

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express()
 const config = require('platformsh-config').config();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World1!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('test')
 })
 
 app.listen(config.port, () => {
