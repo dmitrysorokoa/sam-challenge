1c9978e3690dbede9cb94978321a8f7e1eec84cf
Dmitry Soroko <D.Saroka@sam-solutions.com>
2023-09-29 12:01:02 +0300

    init

--- index.ts
+++ index.ts
@@ -6,15 +6,15 @@ const config = require('platformsh-config').config();
 
 const port = !config.isValidPlatform() ? 3003 : config.port;
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 app.use(express.static("public"));
 
 app.get('/api/test', (req: Request, res: Response) => {
-  res.send({ data: 'test bla1' })
+  res.send({ data: 'test bla12' })
 })
 
 app.use((req: Request, res: Response, next) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
 app.listen(port, () => {
